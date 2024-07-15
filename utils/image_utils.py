276e2c4af9e9026509e1b0629d82da3ec8fe0fdf
Errodion <r.kuzmin.w@gmail.com>
2024-07-15 23:35:55 +0300

    Images compressed settings changed

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -21,19 +21,19 @@ def get_image_path(filename: str):
     return os.path.join('images', f'{filename}.jpg')
 
 
-def compress_image(image_path, target_size_kb=450):
+def compress_image(image_path, target_size_kb=550):
     compressed_path = image_path
 
     while os.path.getsize(compressed_path) > target_size_kb * 1024:
         with Image.open(compressed_path) as img:
             compressed_path = get_modified_filepath(image_path, 'compressed')
-            img.save(compressed_path, format='JPEG', quality=1)
+            img.save(compressed_path, format='JPEG', quality=10)
     return compressed_path
 
 
 def resize_image(image_path):
     min_size = 10
-    max_size = 2560
+    max_size = 5000
 
     with Image.open(image_path) as img:
         img_width = img.width
