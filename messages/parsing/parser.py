da8de3985dd38694186e3a67c43039c8a2eb4da0
Errodion <r.kuzmin.w@gmail.com>
2024-07-15 18:44:18 +0300

    Image preparing for Telegram API

--- messages/parsing/parser.py
+++ messages/parsing/parser.py
@@ -1,15 +1,12 @@
 import os
 
 from messages.message_node import MessageNode
-
-
-def get_image_path(filename: str):
-    return os.path.join('messages', 'parsing', 'images', f'{filename}.jpg')
+from utils.image_utils import prepare_image, get_image_path
 
 
 def parse_message_tree():
     messages_tree = None
-    file_path = os.path.join('messages', 'parsing', 'trees', 'tree.txt')
+    file_path = os.path.join('trees', 'tree.txt')
 
     with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
@@ -25,8 +22,11 @@ def parse_message_tree():
             )
             node_id = parts[0].strip()
             short_text = parts[2].strip()
-            image = get_image_path(parts[3].strip()) if parts[3].strip() else None
-            node = MessageNode(text, short_text, image)
+            image_path = get_image_path(parts[3].strip()) if parts[3].strip() else None
+            if image_path:
+                image_path = prepare_image(image_path)
+
+            node = MessageNode(text, short_text, image_path)
             if messages_tree:
                 messages_tree.add_node(node_id, node)
             else:
