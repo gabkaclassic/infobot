ae982667db0f008e7c5fdd782bd658499c5f8aa5
errodion <r.kuzmin.w@gmail.com>
2024-08-25 15:42:20 +0300

    Redis clients changed: transactions added

--- db/redis/client.py
+++ db/redis/client.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from redis import asyncio as aioredis
+import json, asyncio
 
 load_dotenv()
 
@@ -11,6 +12,7 @@ password = os.environ.get('DB_PASSWORD')
 
 connection_string = f'redis://{host}:{port}'
 
+
 class RedisDatabase:
     def __init__(self, db_number):
         self.redis = aioredis.from_url(
@@ -20,7 +22,7 @@ class RedisDatabase:
 
     async def set_key(self, key, value):
         try:
-            await self.redis.set(key, value)
+            await self.redis.set(key, json.dumps(value))
             return True
         except Exception as e:
             print(e)
@@ -28,22 +30,51 @@ class RedisDatabase:
 
     async def get_key(self, key):
         try:
-            return await self.redis.get(key)
+            value = await self.redis.get(key)
+            return json.loads(value) if value else None
+        except Exception as e:
+            print(e)
+
+    async def delete(self, key):
+        try:
+            return await self.redis.delete(key)
         except Exception as e:
             print(e)
 
     async def close(self):
         await self.redis.close()
 
+
 class UserDatabase(RedisDatabase):
     def __init__(self):
         super().__init__(db_number=1)
 
-    async def add_user(self, client_id: str) -> bool:
-        return await self.set_key(client_id, client_id)
+    async def add_user(self, client_id: str, confirmation_url: str) -> bool:
+        if client_id:
+            return await self.set_key(
+                client_id,
+                {
+                    'confirmation_url': confirmation_url,
+                    'paid': False,
+                }
+            )
+        return False
 
-    async def check_paid(self, client_id: str) -> str:
+    async def get_payment_info(self, client_id: str) -> bool:
         return await self.get_key(client_id)
+
+    async def confirm_payment(self, client_id: str) -> bool:
+        info = await self.get_key(client_id)
+        info['paid'] = True
+        del info['confirmation_url']
+        return await self.set_key(client_id, info)
+
+    async def cancel_payment(self, client_id: str) -> bool:
+        info = await self.get_key(client_id)
+        info['paid'] = False
+        del info['confirmation_url']
+        return await self.set_key(client_id, info)
+
 
 class PaymentDatabase(RedisDatabase):
     def __init__(self):
@@ -53,17 +84,80 @@ class PaymentDatabase(RedisDatabase):
         return await self.set_key(payment_id, client_id)
 
     async def close_payment(self, payment_id: str) -> str:
-        return await self.get_key(payment_id)
+        client_id = await self.get_key(payment_id)
+        await self.delete(payment_id)
+        return client_id
 
 
-payments = PaymentDatabase()
-users = UserDatabase()
+class PaymentManager:
+
+    def __init__(self):
+        self.payments = PaymentDatabase()
+        self.users = UserDatabase()
+
+    async def create_payment(self, client_id: str, payment_id: str, confirmation_url: str) -> str:
+        async with self.payments.redis.client.pipeline(transaction=True) as pipe_payments:
+            pipe_payments.set(payment_id, client_id)
+
+            async with self.users.redis.client.pipeline(transaction=True) as pipe_users:
+                pipe_users.set(client_id, json.dumps({'confirmation_url': confirmation_url, 'paid': False}))
+
+                results_payments, results_users = await asyncio.gather(
+                    pipe_payments.execute(),
+                    pipe_users.execute()
+                )
+
+                if all(results_payments) and all(results_users):
+                    return confirmation_url
+                else:
+                    await self.users.cancel_payment(client_id)
+                    await self.payments.close_payment(payment_id)
+
+    async def confirm_payment(self, client_id: str, payment_id: str) -> bool:
+        async with self.users.redis.client.pipeline(transaction=True) as pipe_users:
+            pipe_users.set(client_id, json.dumps({'paid': True}))
+
+            async with self.payments.redis.client.pipeline(transaction=True) as pipe_payments:
+                pipe_payments.delete(payment_id)
+
+                results_users, results_payments = await asyncio.gather(
+                    pipe_users.execute(),
+                    pipe_payments.execute()
+                )
+
+                return all(results_users) and all(results_payments)
+
+    async def cancel_payment(self, client_id: str, payment_id: str) -> bool:
+        async with self.users.redis.client.pipeline(transaction=True) as pipe_users:
+            pipe_users.set(client_id, json.dumps({'paid': False}))
+
+            async with self.payments.redis.client.pipeline(transaction=True) as pipe_payments:
+                pipe_payments.delete(payment_id)
+
+                results_users, results_payments = await asyncio.gather(
+                    pipe_users.execute(),
+                    pipe_payments.execute()
+                )
+
+                return all(results_users) and all(results_payments)
+
+
+payments = PaymentManager()
+
+
+async def check_db():
+    payments_ping = await payments.payments.redis.ping()
+    assert payments_ping, 'Payments ping failed'
+    users_ping = await payments.users.redis.ping()
+    assert users_ping, 'Users ping failed'
 
 async def initialize_db():
     privileged_users = os.environ.get('PAYMENT_PRIVILEGED_USERS').split(',')
     for user in privileged_users:
-        await users.add_user(user)
+        await payments.users.add_user(user, '')
+        await payments.users.confirm_payment(user)
+
 
 async def close_connections():
-    await payments.close()
-    await users.close()
+    await payments.payments.close()
+    await payments.users.close()
